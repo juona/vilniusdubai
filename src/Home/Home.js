bca0e623ae150a467b2c8973b7750b2814b65c5b
Jonas <jon.biciunas@gmail.com>
2018-07-21 00:42:50 +0300

    Fix box shadows not visible around menu

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -2,22 +2,71 @@ import React from "react";
 import Link from "./Link";
 import styles from "./Home.css";
 
-const Home = () => (
-	<div className={styles.container}>
-		<div className={styles.menuContainer}>
-			<nav className={styles.menu}>
-				<Link to="/photos" className={styles.linkPhotos}>
-					<span className={styles.textNode}>Gallery</span>
-				</Link>
-				<Link to="/map" className={styles.linkMap}>
-					<span className={styles.textNode}>Map</span>
-				</Link>
-				<Link to="/stories" className={styles.linkAbout}>
-					<span className={styles.textNode}>Stories</span>
-				</Link>
-			</nav>
-		</div>
-	</div>
-);
+export default class Home extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { isPhotosHovering: false, isAboutHovering: false };
+		this.toggleLeftShadow = this.toggleLeftShadow.bind(this);
+		this.toggleRightShadow = this.toggleRightShadow.bind(this);
+	}
 
-export default Home;
+	toggleLeftShadow() {
+		this.setState({
+			isPhotosHovering: !this.state.isPhotosHovering
+		});
+	}
+
+	toggleRightShadow() {
+		this.setState({
+			isAboutHovering: !this.state.isAboutHovering
+		});
+	}
+
+	render() {
+		return (
+			<div className={styles.container}>
+				<div
+					className={
+						this.state.isPhotosHovering
+							? styles.hackShadowContainerLeft
+							: styles.hackShadowContainer
+					}
+				>
+					<div className={styles.hackShadow} />
+				</div>
+				<div
+					className={
+						this.state.isAboutHovering
+							? styles.hackShadowContainerRight
+							: styles.hackShadowContainer
+					}
+				>
+					<div className={styles.hackShadow} />
+				</div>
+				<div className={styles.menuContainer}>
+					<nav className={styles.menu}>
+						<Link
+							to="/photos"
+							className={styles.linkPhotos}
+							onMouseEnter={this.toggleLeftShadow}
+							onMouseLeave={this.toggleLeftShadow}
+						>
+							<span className={styles.textNode}>Gallery</span>
+						</Link>
+						<Link to="/map" className={styles.linkMap}>
+							<span className={styles.textNode}>Map</span>
+						</Link>
+						<Link
+							to="/stories"
+							className={styles.linkAbout}
+							onMouseEnter={this.toggleRightShadow}
+							onMouseLeave={this.toggleRightShadow}
+						>
+							<span className={styles.textNode}>Stories</span>
+						</Link>
+					</nav>
+				</div>
+			</div>
+		);
+	}
+}
