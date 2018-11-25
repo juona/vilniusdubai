2b5129920de9db5e34e1825be1bd27ea7ab4d5fc
Jonas <jon.biciunas@gmail.com>
2018-11-25 19:45:35 +0200

    Add keyboard controls to full photo

--- src/Photos/Photos/FullPhoto/FullPhoto.js
+++ src/Photos/Photos/FullPhoto/FullPhoto.js
@@ -5,14 +5,44 @@ import { toggleFullPhoto, displayNextFullPhoto, displayPreviousFullPhoto } from 
 import styles from "./FullPhoto.css";
 import GalleryNavigationButton from "./GalleryNavigationButton";
 
-export const FullPhoto = ({ photoURL, description, onBackgroundClick, onPreviousClick, onNextClick }) => (
-  <div className={photoURL ? styles.background : styles.hidden} onClick={onBackgroundClick}>
-    <img src={photoURL} className={styles.photo} />
-    <span className={styles.caption}>{description}</span>
-    <GalleryNavigationButton onClick={() => onPreviousClick()} />
-		<GalleryNavigationButton onClick={() => onNextClick()} next={true}/>
-  </div>
-);
+export class FullPhoto extends React.Component {
+  constructor(...args) {
+    super(...args);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+  }
+
+  handleKeyDown(e) {
+    if (e.keyCode === 37) {
+      this.props.onPreviousClick();
+    } else if (e.keyCode === 39) {
+      this.props.onNextClick();
+    } else if (e.keyCode === 27) {
+			this.props.onBackgroundClick();
+		}
+  }
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyDown, false);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyDown, false);
+  }
+
+  render() {
+    return (
+      <div
+        className={this.props.photoURL ? styles.background : styles.hidden}
+        onClick={this.props.onBackgroundClick}
+      >
+        <img src={this.props.photoURL} className={styles.photo} />
+        <span className={styles.caption}>{this.props.description}</span>
+        <GalleryNavigationButton onClick={() => this.props.onPreviousClick()} />
+        <GalleryNavigationButton onClick={() => this.props.onNextClick()} next={true} />
+      </div>
+    );
+  }
+}
 
 FullPhoto.propTypes = {
   photoURL: PropTypes.string,
@@ -27,11 +57,9 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-  onBackgroundClick: () => 
-    dispatch(toggleFullPhoto())
-	,
-	onNextClick: () => dispatch(displayNextFullPhoto()),
-	onPreviousClick: () => dispatch(displayPreviousFullPhoto())
+  onBackgroundClick: () => dispatch(toggleFullPhoto()),
+  onNextClick: () => dispatch(displayNextFullPhoto()),
+  onPreviousClick: () => dispatch(displayPreviousFullPhoto())
 });
 
 export default connect(
